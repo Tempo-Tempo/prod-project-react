298259dd258afca975a155d7217473abde1d4854
Tempo-Tempo <4mo.4mo@list.ru>
2023-12-05 22:45:02 +0800

    add tests for login and auth

---
+++ src/features/AuthByUsername/model/slice/LoginSlice.test.ts
@@ -0,0 +1,18 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginScheme } from '../type/LoginScheme';
+import { loginActions, loginReduser } from './LoginSlice';
+
+describe('loginSlice test', () => {
+    test('test username', () => {
+        const state: DeepPartial<LoginScheme> = { username: '12321' };
+        expect(loginReduser(state as LoginScheme, loginActions.setUsername('12321'))).toEqual({ username: '12321' });
+    });
+    test('test password', () => {
+        const state: DeepPartial<LoginScheme> = { password: '123' };
+        expect(loginReduser(state as LoginScheme, loginActions.setPassword('123'))).toEqual({ password: '123' });
+    });
+    test('test username', () => {
+        const state: DeepPartial<LoginScheme> = { username: '12321' };
+        expect(loginReduser(state as LoginScheme, loginActions.setUsername('12321'))).toEqual({ username: '12321' });
+    });
+});
--- src/features/AuthByUsername/services/loginByUsername/loginByUsername.test.ts
+++ src/features/AuthByUsername/services/loginByUsername/loginByUsername.test.ts
@@ -1,35 +1,57 @@
 import { Dispatch } from '@reduxjs/toolkit';
 import { StateSchema } from 'app/providers/StoreProvider';
 import axios from 'axios';
+import { AsyncThunkTest } from 'shared/lib/tests/tests/asyncThunkTests/asyncThunkTest';
 import { loginByUsername } from './loginByUsername';
 
 jest.mock('axios');
 
 const mockValue = jest.mocked(axios, true);
 
-let dispatch: Dispatch;
-let getState: () => StateSchema;
+// let dispatch: Dispatch;
+// let getState: () => StateSchema;
 
-beforeEach(() => {
-    dispatch = jest.fn();
-    getState = jest.fn();
-});
+// beforeEach(() => {
+//     dispatch = jest.fn();
+//     getState = jest.fn();
+// });
+
+// describe('login test', () => {
+//     test('return success', async () => {
+//         const dataValue = { username: '123', id: '1' };
+//         mockValue.post.mockReturnValue(Promise.resolve({ data: dataValue }));
+//         const action = loginByUsername({ username: '123', password: '123' });
+//         const result = await action(dispatch, getState, undefined);
+//         expect(result.payload).toEqual(dataValue);
+//         expect(mockValue.post).toHaveBeenCalled();
+//         expect(dispatch).toBeCalledTimes(3);
+//     });
+//     test('return error', async () => {
+//         mockValue.post.mockReturnValue(Promise.resolve({ status: 403 }));
+//         const action = loginByUsername({ username: '123', password: '123' });
+//         const result = await action(dispatch, getState, undefined);
+//         expect(result.meta.requestStatus).toBe('rejected');
+//         expect(mockValue.post).toHaveBeenCalled();
+//         expect(result.payload).toBe('error');
+//     });
+// });
 
 describe('login test', () => {
     test('return success', async () => {
-        const dataValue = { username: '123', id: '1' };
+        const dataValue = { username: '123', id: '1', password: '123' };
         mockValue.post.mockReturnValue(Promise.resolve({ data: dataValue }));
-        const action = loginByUsername({ username: '123', password: '123' });
-        const result = await action(dispatch, getState, undefined);
+        const thunk = new AsyncThunkTest(loginByUsername);
+        const result = await thunk.callThunk(dataValue);
         expect(result.payload).toEqual(dataValue);
         expect(mockValue.post).toHaveBeenCalled();
-        expect(dispatch).toBeCalledTimes(3);
+        expect(thunk.dispatch).toBeCalledTimes(3);
     });
     test('return error', async () => {
         mockValue.post.mockReturnValue(Promise.resolve({ status: 403 }));
-        const action = loginByUsername({ username: '123', password: '123' });
-        const result = await action(dispatch, getState, undefined);
+        const thunk = new AsyncThunkTest(loginByUsername);
+        const result = await thunk.callThunk({ username: '123', password: '123' });
         expect(result.meta.requestStatus).toBe('rejected');
         expect(mockValue.post).toHaveBeenCalled();
+        expect(result.payload).toBe('error');
     });
 });
